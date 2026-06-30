73162f94168ce3aa3d63bdd091b424ad50154b18
Akshar Mandaliya <aksharmandaliya73@gmail.com>
2026-06-30 13:05:37 +0000

    Commit Configure

---
+++ 01_Basics/DataTypeConversion.js
@@ -0,0 +1,39 @@
+/* let score = "10abc"
+console.log(typeof score);
+let value = Number(score)
+console.log(typeof value)
+console.log(value) */ // Convert String to Integer
+
+/* let ans = 1
+console.log(typeof ans);
+let final = Boolean(ans)
+console.log(typeof final); */ // Convert Integer to Boolean
+
+/* let score = null
+console.log(typeof score);
+let ans = Number(score)
+console.log(typeof ans)
+console.log(ans) // Convert Null Value to Integer(Number)
+ */
+
+/* let score = undefined
+console.log(typeof score);
+let ans = Number(score)
+console.log(typeof ans)
+console.log(ans) */ // Convert Undefined to Number
+
+/* let score = 323
+console.log(typeof score);
+let ans = String(score)
+console.log(typeof ans)
+console.log(ans) */ // Convert Number to String
+
+let name = " "
+console.log(typeof name);
+let ans = Boolean(name)
+console.log(ans);
+console.log(typeof ans); // Convert String to Boolean
+
+
+
+
