ca4e45722764118881d0a726ab35ba2b01f189ab
Akshar Mandaliya <aksharmandaliya73@gmail.com>
2026-07-01 06:48:26 +0000

    configure commit done

---
+++ 01_Basics/Comparision.js
@@ -0,0 +1,29 @@
+/* let nums1 = 10
+let nums2 = 12
+console.log(nums1 > nums2);
+console.log(nums1 >= nums2);
+console.log(nums1 < nums2);
+console.log(nums1 <= nums2);
+console.log(nums1 == nums2);
+console.log(nums1 != nums2);
+ */
+
+/* console.log("2" > 1); // JavaScript Automatically Convert Number 
+console.log(true >= 1) */ // True Means 1
+/* 
+console.log(null == 0);
+console.log(null >= 0);
+console.log(null == 1); */
+
+/* console.log(undefined == 0);
+console.log(undefined >= 0);
+console.log(undefined < 0);
+ */
+
+console.log("2" === 2); // Strict Equality Operator - False
+
+
+
+
+
+
