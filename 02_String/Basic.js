a89f752425bbc2a32ec4d370e13b100beea0e551
Akshar Mandaliya <aksharmandaliya73@gmail.com>
2026-07-01 09:31:31 +0000

    commit done

---
+++ 02_String/Basic.js
@@ -0,0 +1,13 @@
+// String is Nothing But the Sequence of Characters.
+// String is a Primitive Data Type.
+// String Stored in Stack Memory.
+// Once You can stored after can not change.
+// In Java Script Two Ways to Create the String
+
+// Way : 1
+let FirstWay = "Akshsr Mandaliya"
+console.log("First Way : " , FirstWay);
+
+// Way 2 :- Using New Keyword
+let SecondWay = new String("Akshar")
+console.log("Second Way :- " , SecondWay);
---
+++ 02_String/Concate.js
@@ -0,0 +1,7 @@
+let First = "Akshar"
+let Second = " Mandaliya"
+console.log(First + Second); // First Way to Concate String
+
+// Modern Way :- Backticks
+console.log(`My name is ${First} and surname is ${Second}`);
+
