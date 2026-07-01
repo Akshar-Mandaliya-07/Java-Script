87f1e922862a0a261dd42e2f7877053dd9663384
Akshar Mandaliya <aksharmandaliya73@gmail.com>
2026-07-01 05:38:24 +0000

    commit configure completed

---
+++ 01_Basics/Operation.js
@@ -0,0 +1,34 @@
+/* let num1 = 10
+let num2 = 20
+let num3 = num1 + num2
+console.log(num3); // Two Number Addition
+*/
+
+/* let num1 = "Akshar"
+let num2 = 10
+let ans = num1 + num2
+console.log(typeof ans); */
+
+/* let str1 = "Akshar"
+let str2 = " Mandaliya"
+let str3 = str1 + str2
+console.log(str3); */
+
+/* console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 1 + 1); // First Is String Inside Round Bracket so JS Assume Alls Are String and No Any Operation Performed.
+console.log(1 + 1 + "2"); */ // But in this case first is numerical so performed first operation and after join string.
+
+/* console.log(true + 1); // True means 1 :- 1 + 1
+console.log(false + 1); */ // Fasle Means 0 :- 0 + 1
+
+let count = 12;
+++count + count++;
+console.log(count);
+
+
+
+
+
+
+
