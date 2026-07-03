b702508b6afd7dff2e2efb291cf510a88ef71707
Akshar Mandaliya <aksharmandaliya73@gmail.com>
2026-07-03 11:41:52 +0000

    Commit done

--- 01_Basics/Comparision.js
+++ 01_Basics/Comparision.js
@@ -20,7 +20,7 @@ console.log(undefined >= 0);
 console.log(undefined < 0);
  */
 
-console.log("2" === 2); // Strict Equality Operator - False
+console.log("2" === 2); // Strict Equality Operator - False , With Also Check Data Type
 
 
 
