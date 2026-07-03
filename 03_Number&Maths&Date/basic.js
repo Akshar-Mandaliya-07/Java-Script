b6788e753d90bd845555daac9ad57f85a7a72961
Akshar Mandaliya <aksharmandaliya73@gmail.com>
2026-07-03 13:42:05 +0000

    Commit Done

---
+++ 03_Number&Maths&Date/basic.js
@@ -0,0 +1,12 @@
+// In Java Script Declare Number Two Ways.
+const score = 100
+console.log(typeof score); // Type is Number
+
+
+// Second Way is a Using New Keyword
+const number = new Number(100)
+console.log(typeof number); // Type is Object
+
+
+
+
