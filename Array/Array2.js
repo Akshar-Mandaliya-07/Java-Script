cd06d0c8a852f2c8b671dbf21b5619c840b045aa
Akshar Mandaliya <aksharmandaliya73@gmail.com>
2026-07-04 07:41:40 +0000

    commit done

---
+++ Array/Array2.js
@@ -0,0 +1,20 @@
+let MyArray1 = [1,2,3,4,5]
+let MyArray2 = [6,7,8,9,10]
+// MyArray1.push(MyArray2)
+// console.log(MyArray1[5]);
+
+let MyArray3 = MyArray1.concat(MyArray2)
+// console.log(MyArray3);
+
+// let MyArray4 = [... MyArray1 , ... MyArray2]
+// console.log(MyArray4);
+
+/* const otherArray = [1,2,3,[4,5,[6,7,8,[9,10]]]]
+let MyRealArray = otherArray.flat(Infinity)
+console.log(MyRealArray); */
+
+console.log(Array.isArray([1,2,3])); // Check if Array or Not
+console.log(Array.from("Akshar")); // String Convert to Array Element
+
+
+
