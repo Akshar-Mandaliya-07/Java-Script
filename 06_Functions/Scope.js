af0f821d78c0830c1d148c48017ebb797f577807
Akshar Mandaliya <aksharmandaliya73@gmail.com>
2026-07-05 13:48:03 +0000

    Commit done

---
+++ 06_Functions/Scope.js
@@ -0,0 +1,32 @@
+function MyFirstFunction(){
+    const id = 1
+    function MySecondFunction(){
+        const name = "Akshar Mandaliya"
+        console.log("Outside Function :- " ,id);        
+    }
+    //console.log("Name is :- " , name) // Outer Function can not access the inside function properties.
+    MySecondFunction()
+}
+// MyFirstFunction()
+
+if(true){
+    const name = "Akshar"
+    if(name === "Akshar"){
+        const id =  1
+        // console.log(id + name);
+    }
+    // Error :-  console.log("Inside Function :- ",id); 
+}
+// Error :- console.log("Outside Function :- ",name);
+
+// Create Function Has Two Ways :- 
+console.log(MyNewFunction1(5));
+function MyNewFunction1(num){
+    return num + 1
+}
+
+//console.log(ans(5)); // Can not Use Ans Function Before Function Initialization
+const ans = function(value){
+    return value + 1
+}
+
