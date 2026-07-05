16789407f7ae2766e33e64ad131699079351b64e
Akshar Mandaliya <aksharmandaliya73@gmail.com>
2026-07-05 07:18:23 +0000

    Commit done

---
+++ 05_Object/Object3.js
@@ -0,0 +1,14 @@
+const Myobject1 = {
+    id : 1,
+    name : "Akshar Mandaliya",
+    email : "akshar@google.com",
+    city : "Mumbai"
+}
+console.log(Object.keys(Myobject1));
+console.log(Object.values(Myobject1));
+// console.log(Object.);
+
+
+
+
+
