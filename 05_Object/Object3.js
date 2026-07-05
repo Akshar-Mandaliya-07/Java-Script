299e1401634b119558efeb746af866fcb4c5236c
Akshar Mandaliya <aksharmandaliya73@gmail.com>
2026-07-05 08:48:08 +0000

    Commit done

--- 05_Object/Object3.js
+++ 05_Object/Object3.js
@@ -4,9 +4,18 @@ const Myobject1 = {
     email : "akshar@google.com",
     city : "Mumbai"
 }
-console.log(Object.keys(Myobject1));
-console.log(Object.values(Myobject1));
+// console.log(Object.keys(Myobject1));
+// console.log(Object.values(Myobject1));
 // console.log(Object.);
+
+// Java Script Allow Duplicate Keys
+const MyDuplictaeObject = {
+    id : 1,
+    id : 2,
+    id : 3
+}
+console.log(MyDuplictaeObject);
+
 
 
 
