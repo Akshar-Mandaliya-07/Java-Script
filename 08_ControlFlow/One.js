5734aae862b4055788b994b7eb1d8f615b3e942d
Akshar Mandaliya <aksharmandaliya73@gmail.com>
2026-07-07 13:16:48 +0000

    Commit done

---
+++ 08_ControlFlow/One.js
@@ -0,0 +1,24 @@
+// IF
+if("2" === 2){
+    console.log("Condition True");
+}
+
+// If-Else
+if(10 > 20){
+    console.log("Greater");
+}
+else{
+    console.log("Less Than");
+}
+
+// If-Else if
+let a = 20
+if(a > 30){
+    console.log("A is Greater Than");
+}
+else if(a < 30){
+    console.log("A is Less Than");
+}
+else{
+    console.log("A is Zero");
+}
