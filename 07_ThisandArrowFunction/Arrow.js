29271c9d78338fa24af5ba2505c5ab4f35a2a2f1
Akshar Mandaliya <aksharmandaliya73@gmail.com>
2026-07-07 13:06:05 +0000

    commit done

---
+++ 07_ThisandArrowFunction/Arrow.js
@@ -0,0 +1,19 @@
+/* const MyFunction = function(){
+    console.log(MyFunction);
+    
+}
+console.log(MyFunction);
+ */
+
+
+/* const MyFunction = (num1,num2) => {
+    return num1 + num2
+}
+console.log(MyFunction(1,2)); */
+
+/* const MyFunction = (num1,num2) => (num1 + num2)
+console.log(MyFunction(1,2)) */
+
+const AddTwo = (num1,num2) => ({username : "Akshar"})
+console.log(AddTwo(1,2));
+
---
+++ 07_ThisandArrowFunction/IIFE.js
@@ -0,0 +1,18 @@
+// Immediately Invoked Function Expression
+/* function MyFunction()
+{
+    console.log("Hello , I am Akshar")
+}
+MyFunction()
+*/
+
+(function MyFunction()
+{
+    console.log("Hii , MyFunction")
+})();
+
+(() => {
+    console.log("Inside Arrow Function");
+})()
+
+
