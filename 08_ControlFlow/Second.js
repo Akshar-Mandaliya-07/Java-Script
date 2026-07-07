8f45b72f58513262898102b5a14d2c39064cfba1
Akshar Mandaliya <aksharmandaliya73@gmail.com>
2026-07-07 13:28:51 +0000

    Commit done

---
+++ 08_ControlFlow/Second.js
@@ -0,0 +1,30 @@
+// For Loop
+for(let a = 1 ; a <= 10 ; a++){
+    // console.log(a);
+}
+
+// While Loop
+let b = 1
+while(b <= 10){
+    // console.log(b);
+    b++
+}
+
+// do-while loop
+let c = 1
+do{
+    // console.log(c);
+    c++
+}while(c <= 10);
+
+// For-Each loop
+let arr1 = [1,2,3,4]
+let arr2 = []
+arr1.forEach(arr1 => {
+    arr2.push(arr1)
+});
+console.log(arr2);
+
+
+
+
