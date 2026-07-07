83fb357ba182d44d96953c8313ebae29031c693f
Akshar Mandaliya <aksharmandaliya73@gmail.com>
2026-07-07 13:31:36 +0000

    commit done

---
+++ 08_ControlFlow/Third.js
@@ -0,0 +1,16 @@
+// Switch-Case 
+
+let choice = 10
+switch(choice){
+    case 1:
+        console.log("Sunday");
+        break;
+    case 2:
+        console.log("Monday");
+        break;
+    case 3:
+        console.log("Tuesday");
+        break;
+    default:
+        console.log("Enter Valid Choice");        
+}
