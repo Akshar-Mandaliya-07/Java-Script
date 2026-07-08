2de8cb7c332e6a8d29a17f1435ee41a6b683247c
Akshar Mandaliya <aksharmandaliya73@gmail.com>
2026-07-08 02:11:41 +0000

    commit done

--- 08_ControlFlow/Seven.js
+++ 08_ControlFlow/Seven.js
@@ -1,8 +1,12 @@
-const arr = [1,2,3,4,5]
-arr.forEach( function (val){
-    console.log(val);
-} )
+// For Each Loop
+
+const arr = [1,2,3,4,5,6,7,8,9,10]
+// console.log(typeof arr);
+
 arr.forEach( (item) => {
     console.log(item);
-    
-});
+})
+
+arr.forEach( function (item) {
+    console.log(item);
+})
