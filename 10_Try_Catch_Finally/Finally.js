ff9cd55e3aabb53562535607c4bf4694c8d7730e
Akshar Mandaliya <aksharmandaliya73@gmail.com>
2026-07-10 15:48:07 +0000

    commit done

---
+++ 10_Try_Catch_Finally/Finally.js
@@ -0,0 +1,31 @@
+// console.log(a);
+// try {
+//     console.log(a);
+// }
+// catch (error) {
+//     console.log(error.message);
+// }
+
+// finally {
+//     console.log("Code Execution Complete");
+// }
+
+
+
+const MyFunction = () => {
+    try {
+        //console.log(a);
+        // console.log("Code Exection Successfully....");
+        return
+    }
+    catch (error) {
+        // console.log(error.message);
+    }
+
+    finally {
+        // console.log("Code Execution Complete");
+    }
+}
+MyFunction()
+// console.log("End Execution ...");
+
