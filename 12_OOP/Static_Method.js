a07b6d42ceb47c07dafb098d1213e1b34487f21b
Akshar Mandaliya <aksharmandaliya73@gmail.com>
2026-07-11 06:23:54 +0000

    commit done

---
+++ 12_OOP/Static_Method.js
@@ -0,0 +1,24 @@
+class MyClass
+{
+    static addition(a , b)
+    {
+        return a + b
+    }
+}
+console.log(MyClass.addition(1,2))
+
+
+
+// *****************Getter and Setter *****************
+// class demo{
+//     setName(name){
+//         this.name = name
+//     }
+//     getName(){
+//         return this.name
+//     }
+// }
+// let result = new demo()
+// result.setName("Akshar")
+// console.log(result.getName());
+
